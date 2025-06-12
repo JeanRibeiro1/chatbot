bc5e6a1829ed91faf104f3ae3d5bcfbaecaea700
Jean Pereira Ribeiro <31575143+JeanRibeiro1@users.noreply.github.com>
2025-06-12 20:31:11 -0300

    Adiciona pausa para esperar DB e corrige migração remota

--- release.sh
+++ release.sh
@@ -3,6 +3,10 @@
 # O comando 'set -e' garante que o script irá parar imediatamente se algum comando falhar.
 set -e
 
+# Pausa por 5 segundos para dar tempo ao banco de dados de iniciar.
+echo "Aguardando 5 segundos para o banco de dados iniciar..."
+sleep 5
+
 # 1. Baixa os pacotes do NLTK
 python download_nltk.py
 
